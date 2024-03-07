b62be6bb14a520675bdc456aed4840805355dde5
unknown <borigabor08@gmail.com>
2024-03-07 09:14:26 +0100

    Add GameBoad component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import GameBoard from './Components/GameBoard/GameBoard';
 import Menu from './Components/Menu/Menu';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Thi is the App component</h3>
-        <Menu/>
-      </header>
-    </div>
-  );
+export default function App() {
+  return(
+    <>
+    <Menu />
+    <GameBoard />
+    </>
+  )
 }
-
-export default App;
---
+++ src/Components/GameBoard/GameBoard.jsx
@@ -0,0 +1,7 @@
+import './GameBoard.css';
+
+export default function GameBoard() {
+    return(
+        <><h3>Thi is the Game Board Component</h3></>
+    )
+}
